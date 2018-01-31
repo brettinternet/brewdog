9eeb627efd61c977f99fe9fa68f0843a13ae57d6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-31 18:20:11 +0000

    Deploy brettinternet/brewdog to github.com/brettinternet/brewdog.git:gh-pages

--- app/actions/ajaxStatusActions.js
+++
@@ -1,9 +0,0 @@
-import { BEGIN_AJAX_CALL, AJAX_CALL_ERROR } from './constants';
-
-export function beginAjaxCall() {
-  return { type: BEGIN_AJAX_CALL };
-}
-
-export function ajaxCallError() {
-  return { type: AJAX_CALL_ERROR };
-}
